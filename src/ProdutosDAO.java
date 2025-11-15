3b63e892db71b6102d0f0d3ca7807e60ab87ec73
Carlos Borges <kaca_master@hotmail.com>
2025-11-15 20:21:39 -0300

    Listagem de produtos cadastrado

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,7 +7,6 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -15,42 +14,62 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
 
+    public void cadastrarProduto(ProdutosDTO produto) {
 
         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
-        
-        try{
-        conn = new conectaDAO().connectDB();
-         prep = conn.prepareStatement(sql); 
-           
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+
             prep.setString(1, produto.getNome());
-            prep.setInt(2,produto.getValor());
+            prep.setInt(2, produto.getValor());
             prep.setString(3, produto.getStatus());
 
             prep.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
 
-        }catch(SQLException e) {
-
+        } catch (SQLException e) {
 
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Erro ao salvar produto: " + e.getMessage());
         }
-        
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+        String sql = "SELECT * FROM produtos";
+
         
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+
+                ProdutosDTO p = new ProdutosDTO();
+                p.setId(resultset.getInt("id"));
+                p.setNome(resultset.getString("nome"));
+                p.setValor(resultset.getInt("valor"));
+                p.setStatus(resultset.getString("status"));
+                listagem.add(p);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar filme por Código!\n" + e.getMessage());
+
+        }
         return listagem;
     }
 
 }
-
